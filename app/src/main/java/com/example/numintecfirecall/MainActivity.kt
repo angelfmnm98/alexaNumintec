4d9b9b2f0cd53a88bda9b545be48362ebf88d18a
angelfmnm98 <angel.franco@telavox.com>
2024-07-16 13:36:54 +0200

    demo videocall

--- app/src/main/java/com/example/numintecfirecall/MainActivity.kt
+++ app/src/main/java/com/example/numintecfirecall/MainActivity.kt
@@ -12,37 +12,43 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
 import com.example.numintecfirecall.ui.theme.NumintecFireCallTheme
+import android.content.Intent
+import android.net.Uri
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-class MainActivity : ComponentActivity() {
+
+
+class MainActivity : AppCompatActivity() {
+
     @OptIn(ExperimentalTvMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NumintecFireCallTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    shape = RectangleShape
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val buttonVideoCall: Button = findViewById(R.id.button_video_call)
+        buttonVideoCall.setOnClickListener {
+            val url = "https://numintec-test.daily.co/Numintec_it"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
         }
     }
-}
 
-@OptIn(ExperimentalTvMaterial3Api::class)
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @OptIn(ExperimentalTvMaterial3Api::class)
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NumintecFireCallTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        NumintecFireCallTheme {
+            Greeting("Android")
+        }
     }
 }
